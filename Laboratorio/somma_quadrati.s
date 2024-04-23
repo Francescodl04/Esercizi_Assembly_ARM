755406266675ed7c49e39c08e8759c1be7c825f1
Francesco Di Lena <72445602+Francescodl04@users.noreply.github.com>
2024-04-23 13:46:48 +0200

    Caricato file di laboratorio 23-04-2024

---
+++ Laboratorio/somma_quadrati.s
@@ -0,0 +1,41 @@
+.data
+   addr_n: .word 10		@ Word in memoria con il valore di n 
+.bss
+   addr_sum: .skip 4		@ Word in memoria dove salvare il valore finale
+.text
+   .global main
+main:
+   @Contenuto dei vari registri:
+   @ r0: contiene la somma parziale
+   @ r1: contiene il contatore
+   @ r2: contiene il quadrato calcolato in ogni iterazione (r2 = r1*r1)
+   @ r3: contiene il valore n
+   @ r4: contiene l'indirizzo in memoria di n
+   @ r5: contiene l'indirizzo in memoria di sum.
+   
+   @@@ Inizializzazione variabili e ciclo for
+   
+   MOV R0, #0
+   MOV R1, #1
+   MOV R2, #0
+   LDR R4, =addr_n
+   LDR R3, [R4]
+   LDR R5, =addr_sum
+   B loop
+   
+loop:
+     @@@ Parte principale del loop 
+     MUL R2, R1, R1
+     ADD R0, R0, R2
+     ADD R1, R1, #1
+     @@@ Verifica fine ciclo for: 
+     CMP R1, R3
+     @@@  1. salto a loop per ripetere
+     BLS loop
+     @@@  2. oppure continua dritto se il ciclo for è terminato
+     
+  
+   @@@ Parte finale del programma
+   
+   STR R0, [R5]
+   MOV PC, LR 
